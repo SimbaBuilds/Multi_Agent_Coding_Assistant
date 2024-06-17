dd378c644ceb6702a8d525ab5f1483d5dc5f0c26
Cameron Hightower <cameronhightower@Camerons-MacBook-Air.local>
2024-06-16 23:11:45 -0700

    Auto run

--- question_retrieval/scratch.py
+++ question_retrieval/scratch.py
@@ -2,13 +2,14 @@ from interpreter import interpreter
 
 interpreter.llm.api_key = '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'
 interpreter.llm.model = "gpt-4o"
+# interpreter.model = "gpt-3.5-turbo"
 interpreter.llm.context_window = 128000
 interpreter.llm.max_tokens = 3000
-
-interpreter.chat("Hello")  # Starts an interactive chat
-
+interpreter.auto_run = True
+interpreter.chat()  # Starts an interactive chat
+# interpreter.chat("Hello")  
 
 # print(interpreter.messages)
 # interpreter.messages = []
 
-print(interpreter.messages[-1]["content"])
+# print(interpreter.messages[-1]["content"])
